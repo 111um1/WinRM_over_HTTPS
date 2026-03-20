4330ddb4cc6fa50be09266a8478f653796120057
111um1 <84978246+111um1@users.noreply.github.com>
2026-03-20 17:43:26 +0100

    Create server.ps1

---
+++ server.ps1
@@ -0,0 +1,44 @@
+$FQDN = try { [System.Net.Dns]::GetHostEntry($env:COMPUTERNAME).HostName } catch { $env:COMPUTERNAME }
+
+# Création d’un cert serveur renforcé pour WinRM HTTPS (15 ans)
+$cert = New-SelfSignedCertificate `
+  -DnsName $FQDN, $env:COMPUTERNAME `
+  -CertStoreLocation 'Cert:\LocalMachine\My' `
+  -NotAfter (Get-Date).AddYears(50) `
+  -KeyAlgorithm RSA `
+  -KeyLength 4096 `
+  -HashAlgorithm SHA256 `
+  -KeyExportPolicy NonExportable `
+  -KeyUsage DigitalSignature, KeyEncipherment `
+  -TextExtension @(
+      # EKU = Server Authentication
+      '2.5.29.37={text}1.3.6.1.5.5.7.3.1',
+      # Basic Constraints: CA=false
+      '2.5.29.19={critical}{text}CA=FALSE'
+  ) `
+  -Provider 'Microsoft Enhanced RSA and AES Cryptographic Provider'
+
+# Supprime l’eventuel listener HTTP si tu veux du full HTTPS
+Remove-Item -Path WSMan:\Localhost\Listener\Listener_*\ -Recurse -Force
+
+# Crée le listener HTTPS
+New-Item -Path WSMan:\Localhost\Listener `
+  -Transport HTTPS `
+  -Address * `
+  -Hostname $FQDN `
+  -CertificateThumbprint $cert.Thumbprint | Out-Null
+
+# Vérifications
+Get-ChildItem WSMan:\Localhost\Listener
+winrm enumerate winrm/config/listener
+
+
+
+
+
+# $certi = $cert.Thumbprint
+# $com = $env:COMPUTERNAME
+# $cert = Get-ChildItem Cert:\LocalMachine\My\$certi
+# test-wsman xXXX -UseSSL
+# Export-Certificate -Cert $cert -FilePath C:\temp\$com.cer
+# Import-Certificate -FilePath .\winrm-server.cer -CertStoreLocation Cert:\LocalMachine\Root
