f6b78a89e633f0ea31c33e260b1cc4b6ea2cebe1
111um1 <84978246+111um1@users.noreply.github.com>
2026-03-23 16:45:33 +0100

    Update client_side.ps1

--- client_side.ps1
+++ client_side.ps1
@@ -1,9 +1,9 @@
 <#
 
-# Test WinRM access
-test-wsman SERVERNAME -UseSSL
-
 # Import certificate previously export
 Import-Certificate -FilePath .\winrm-server.cer -CertStoreLocation Cert:\LocalMachine\Root
+
+# Test WinRM access
+test-wsman SERVERNAME -UseSSL
 
 #>
