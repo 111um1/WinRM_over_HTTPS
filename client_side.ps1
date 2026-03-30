d81d14df60777ac0fa93fe90ace810b0e6202e0f
111um1 <84978246+111um1@users.noreply.github.com>
2026-03-30 14:07:17 +0200

    Update client_side.ps1

--- client_side.ps1
+++ client_side.ps1
@@ -11,4 +11,18 @@ Invoke-Command -ComputerName SERVERNAME -UseSSL -ScriptBlock { ipconfig }
 Invoke-Command -ComputerName (Get-Content .\servers.txt) -UseSSL -ScriptBlock { ipconfig }
 ``
 
+# Check if domain firewall is enabled
+Invoke-Command -ComputerName (Get-Content .\servers_sage.txt) -UseSSL -ScriptBlock {
+  Get-NetFirewallProfile | ForEach-Object {
+    [pscustomobject]@{
+      Hostname = $env:COMPUTERNAME
+      Profil   = $_.Name
+      Actif    = $_.Enabled
+    }
+  }
+} |
+Sort-Object Hostname, Profil |
+Where-Object { $_.Profil -eq "Public" -and $_.Actif -eq $false } |
+Format-Table -AutoSize
+
 #>
